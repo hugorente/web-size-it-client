725057b2d089c08ed914b6eafd00cacc215d9112
GregorioBaldari <gregorio.baldari@gmail.com>
2015-10-30 14:07:12 +0000

    bug fixing

--- client/js/app.js
+++ client/js/app.js
@@ -23,34 +23,40 @@ mainApp.service('appVars', function () {
     };
 });
 
+//Change socket configuration to link to server on local or remote.
+//To use server on local use 'node server.js' command in the local server project folder
 mainApp.factory('socket', ['$rootScope', function ($rootScope) {
-  var socket = io('https://secret-lake-6472.herokuapp.com/');
-  //var socket = io('http://localhost:3000');
-  return {
-    on: function (eventName, callback) {
-        function wrapper() {
-            var args = arguments;
-            $rootScope.$apply(function () {
-                callback.apply(socket, args);
+    //The following namespace is used on server side.
+    //TO DO
+    //Let's the user create a namespace, register it on server, and connet to it
+    var projectSpace = 'projectSpace';
+    //var socket = io('https://secret-lake-6472.herokuapp.com/');
+    var socket = io('http://localhost:3000/' + projectSpace);
+    return {
+        on: function (eventName, callback) {
+            function wrapper() {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            }
+        
+            socket.on(eventName, wrapper);
+
+            return function () {
+                socket.removeListener(eventName, wrapper);
+            };
+        },
+
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if(callback) {
+                        callback.apply(socket, args);
+                    }
+                });
             });
         }
-        
-        socket.on(eventName, wrapper);
-        
-        return function () {
-            socket.removeListener(eventName, wrapper);
-        };
-    },
-
-    emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
-            var args = arguments;
-            $rootScope.$apply(function () {
-                if(callback) {
-                    callback.apply(socket, args);
-                }
-            });
-        });
-    }
-  };
+    };
 }]);
